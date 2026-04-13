c5d45a26fe40e891e63b07ff1accd1c8238939f3
Rishabh7985 <rishabhksingh2004@gmail.com>
2026-04-13 22:26:53 +0530

    commit

--- Day-01/basics.js
+++ Day-01/basics.js
@@ -1,1 +1,17 @@
 // This is JS basics
+// let const var
+
+
+//* 1. Let 
+let admin ="Rishabh";
+// console.log(admin);
+admin ="Ris";// This is allowed
+console.log(admin);
+
+//* 2. var:-- Don't use var
+var new_name="rks";
+console.log(new_name);
+
+//* 3. Const :- This can't be redeclarable late only be used one at time 
+const naming ="variables";
+console.log(naming);
