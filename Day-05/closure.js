1110c681ff47f07b0fb4f3847775ea86c348c297
Rishabh7985 <rishabhksingh2004@gmail.com>
2026-04-24 17:19:19 +0530

    Commit

--- Day-05/closure.js
+++ Day-05/closure.js
@@ -1,17 +1,31 @@
-// Lexical scope 
-function creatCounter(){
-    let count =0;
+// Lexical scope
 
-    return function(){
-        count++;
-        return count;
-    }
+function outerfunction() {
+  let new_name = "Rks";
+  console.log(new_name2); // This will give you reference error as new_name2 is not defined
+
+  function innerfunction() {
+    console.log(new_name); // Here you can access the new_name variable as its like child and variable is parent but vice versa is not possible not even in sibllings function 
+    let new_name2 = "new Rks";
+  }
+  innerfunction();
 }
 
-
-
-
-
-
+outerfunction();
 
 // Clousers
+
+function outer() {
+  let secret = "This is outer secret";
+
+  function inner() {
+    console.log(secret);
+  }
+  return inner;
+  console.log("Hellowww");
+}
+
+// outer();// no resul
+
+const fn = outer();
+fn();
