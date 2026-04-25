4a02463e9c19402153fa0c39215bec492608a360
Rishabh7985 <rishabhksingh2004@gmail.com>
2026-04-25 22:51:52 +0530

    Commit

--- Day-05/closure.js
+++ Day-05/closure.js
@@ -5,7 +5,7 @@ function outerfunction() {
   console.log(new_name2); // This will give you reference error as new_name2 is not defined
 
   function innerfunction() {
-    console.log(new_name); // Here you can access the new_name variable as its like child and variable is parent but vice versa is not possible not even in sibllings function 
+    console.log(new_name); // Here you can access the new_name a new  variable as its like child and variable is parent but vice versa is not possible not even in sibllings function 
     let new_name2 = "new Rks";
   }
   innerfunction();
